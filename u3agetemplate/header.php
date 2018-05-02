a77c2f7f7ca0130e551340e12bf26777b846adf8
Kyle-Hess <kyle.hess@my.jcu.edu.au>
2018-05-02 19:17:43 +1000

    -added option for header image -logo flex's to the left of title -header colour change

--- u3agetemplate/header.php
+++ u3agetemplate/header.php
@@ -13,51 +13,65 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'u3agetemplate' ); ?></a>
+<figure id="page" class="site">
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'u3agetemplate'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$u3agetemplate_description = get_bloginfo( 'description', 'display' );
-			if ( $u3agetemplate_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $u3agetemplate_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <?php if (get_header_image() && is_frontpage()) : ?>
+        <figure class="header-image">
+            <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+                <img src="<?php header_image(); ?>" width="<?php echo absint(get_custom_header()->width); ?>"
+                     height="<?php echo absint(get_custom_header()->height); ?>"
+                     alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
+            </a>
+        </figure>
+    <?php endif; ?> <!--header Image -->
+
+    <header id="masthead" class="site-header">
+        <div class="site-branding">
+            <?php the_custom_logo();
+
+            if (is_front_page() && is_home()) : ?>
+            <div class="site-branding-text">
+                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                          rel="home"><?php bloginfo('name'); ?></a></h1>
+                <?php
+                else :
+                    ?>
+                    <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                             rel="home"><?php bloginfo('name'); ?></a></p>
+                <?php
+                endif;
+                $u3agetemplate_description = get_bloginfo('description', 'display');
+                if ($u3agetemplate_description || is_customize_preview()) :
+                    ?>
+                    <p class="site-description"><?php echo $u3agetemplate_description; /* WPCS: xss ok. */ ?></p>
+                <?php endif; ?>
+            </div>
+        </div><!-- .site-branding -->
 
         <div class="login-sign-up">
             <a>Login</a><span> / </span>
             <a>Sign up</a>
         </div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'u3agetemplate' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        <nav id="site-navigation" class="main-navigation">
+            <button class="menu-toggle" aria-controls="primary-menu"
+                    aria-expanded="false"><?php esc_html_e('Primary Menu', 'u3agetemplate'); ?></button>
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'menu-1',
+                'menu_id' => 'primary-menu',
+            ));
+            ?>
+        </nav><!-- #site-navigation -->
+    </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
